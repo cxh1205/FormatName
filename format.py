b3970ee7ce1b87f1f2d354f06ff4c515a7985e0b
cxh <401158573@qq.com>
2022-05-19 22:18:04 +0800

    v2.4.2

--- format.py
+++ format.py
@@ -12,35 +12,46 @@ app.config["SEND_FILE_MAX_AGE_DEFAULT"] = timedelta(seconds=1)
 HOST_PAGE = "http://localhost:40115"
 HOST = "127.0.0.1"
 PORT = 40115
-VERSION = "v2.4.1"
+VERSION = "v2.4.2"
 
 
 class Excel_List:
-    def __init__(self, path):
+
+    def __init__(self, path):  # 读取表格
         self.path = path.strip('"')
         self.sheet = openpyxl.load_workbook(self.path, data_only=True).active
 
-    def is_correct_excel(self):
-        for i in range(1, 1000):
-            if self.sheet.cell(i, 2).value != None:
-                self.find_line_range()
-                self.find_col_range()
-                return 1
-        return 0
+    def is_correct_excel(self):  # 判断表格格式是否正确
+        if self.sheet.cell(1, 1).value == None:
+            return 0
+        if self.sheet.max_column <= 1:  # 单列表格
+            self.first_line = 1
+            self.find_last_line()
+            if self.last_line<2:
+                return -1
+            self.first_col = 1
+            self.last_col = 1
+            return 1
+        elif self.sheet.max_column > 1:  # 多列表格
+            for i in range(1, 10):  # 判断是否存在表格标题
+                if self.sheet.cell(i, 2).value != None:
+                    self.first_line = i
+                    self.find_last_line()
+                    if self.last_line<2:
+                        return -1
+                    self.first_col = 1
+                    self.find_last_col()
+                    return 1
 
-    def find_line_range(self):
-        for i in range(1, 5):
-            if self.sheet.cell(i, 2).value != None:
-                break
-        self.first_line = i
+    def find_last_line(self):
+        i = self.first_line
         while 1:
             if self.sheet.cell(i, 1).value == None:
                 break
             i += 1
         self.last_line = i - 1
 
-    def find_col_range(self):
-        self.first_col = 1
+    def find_last_col(self):
         i = 1
         while 1:
             if self.sheet.cell(self.first_line, i).value == None:
@@ -48,13 +59,16 @@ class Excel_List:
             i += 1
         self.last_col = i - 1
 
-    def return_col_data(self, col_num):
+    def return_col_data(self, col_num):  # 返回一列的数据并判断是否适合做关键字
         col = {}
-        col["key"] = self.sheet.cell(self.first_line, col_num).value
+        col["key"] = self.sheet.cell(self.first_line,
+                                     col_num).value  # 关键字是该列的第一行
         col["values"] = [
             self.sheet.cell(i, col_num).value
             for i in range(self.first_line + 1, self.last_line + 1)
-        ]
+        ]  # 表里的值是表格除了标题以外的东西
+
+        #判断表格值是否包含重复项
         col["delta"] = len(col["values"]) - len(set(col["values"]))
         if col["delta"] > 2:
             col["isKeyWord"] = False
@@ -62,11 +76,16 @@ class Excel_List:
         else:
             col["isKeyWord"] = True
             col["reason"] = "该项适合做关键字"
+        del col["delta"]
+
+        #判断表格值是否为序号
+
         return col
 
-    def return_excel_data(self):
+    def return_excel_data(self):  # 构成关键字表格
         return [
-            self.return_col_data(x) for x in range(self.first_col, self.last_col + 1)
+            self.return_col_data(x)
+            for x in range(self.first_col, self.last_col + 1)
         ]
 
     def show_excel(self):
@@ -78,7 +97,7 @@ class Excel_List:
             for i in sheet:
                 print(i["values"][j], end="\t")
             print()
-        print(j)
+        print("共%d行" % j + 1)
 
 
 def read_json(path):
@@ -135,22 +154,29 @@ def log():
 
 @app.route("/SubmitExcelPath", methods=["post"])
 def submit_excel_path():
-    path = os.path.normpath(json.loads(request.data.decode())["path"].strip('"'))
+    path = os.path.normpath(
+        json.loads(request.data.decode())["path"].strip('"'))
     if not os.path.exists(path):
         return jsonify({"code": 1, "msg": "提交失败，文件不存在"})
     if not re.search(
-        '^[A-Za-z]:\\\\([^|><?*":\\/]*\\\\)*[^|><?*":\\/]*\.((xlsx)|(xlsm)|(xltx)|(xltm))$',
-        path,
+            '^[A-Za-z]:\\\\([^|><?*":\\/]*\\\\)*[^|><?*":\\/]*\.((xlsx)|(xlsm)|(xltx)|(xltm))$',
+            path,
     ):
-        return jsonify(
-            {"code": 2, "msg": "提交失败，不能读取该格式文件，请选择(.xlsx)(.xlsm)(.xltx)(.xltm)文件"}
-        )
+        return jsonify({
+            "code":
+            2,
+            "msg":
+            "提交失败，不能读取该格式文件，请选择(.xlsx)(.xlsm)(.xltx)(.xltm)文件"
+        })
     sheet = Excel_List(path)
-    if sheet.is_correct_excel():
+    code=sheet.is_correct_excel()
+    if code==1:
         config["data"] = sheet.return_excel_data()
         return json.dumps({"code": 0, "msg": "读取成功"}, ensure_ascii=False)
-    else:
-        return json.dumps({"code": 3, "msg": "文件不规范，请阅读说明"}, ensure_ascii=False)
+    elif code==0:
+        return json.dumps({"code": 3, "msg": "文件不能为空"}, ensure_ascii=False)
+    elif code==-1:
+        return json.dumps({"code": 4, "msg": "文件应至少有2行"}, ensure_ascii=False)
 
 
 @app.route("/SubmitData", methods=["post"])
@@ -167,9 +193,8 @@ def submit_data():
 def submit_execute():
     a = json.loads(request.data.decode())
     a["path"] = os.path.normpath(a["path"].strip('"'))
-    if not re.search(
-        '^[A-Za-z]:\\\\([^|><?*":\\/]*\\\\)*([^|><?*":\\/]*)?$', a["path"]
-    ):
+    if not re.search('^[A-Za-z]:\\\\([^|><?*":\\/]*\\\\)*([^|><?*":\\/]*)?$',
+                     a["path"]):
         return jsonify({"code": 4, "msg": "提交失败，不是路径的标准格式"})
     if a["path"] in MY_PATH:
         return jsonify({"code": 3, "msg": "提交失败，请不要提交包含本程序的路径"})
@@ -189,7 +214,8 @@ def return_old_and_new_name_compare():
     os.chdir(execute["path"])
     execute["old"] = [x for x in os.listdir() if os.path.isfile(x)]
     execute["data"] = [
-        x for x in sorted(config["data"], key=lambda x: x["delta"]) if x["isKeyWord"]
+        x for x in sorted(config["data"], key=lambda x: x["delta"])
+        if x["isKeyWord"]
     ]
     return_new_name_list()
     find_new_name()
@@ -219,11 +245,11 @@ def find_new_name():
         for j in execute["data"]:  # 遍历所有关键字，j为关键字的键值对
             for k in range(len(j["values"])):  # 遍历该关键字的值列表，k为值的序数
                 # if not execute['map'][k]:#只要匹配到了一次就不匹配下一个关键字
-                if re.search(str(j["values"][k]), execute["old"][i]):  # 在旧名字里匹配关键字
+                if re.search(str(j["values"][k]),
+                             execute["old"][i]):  # 在旧名字里匹配关键字
                     if execute["map"][k] > 0:
-                        new_name = (
-                            execute["new"][k] + "(%s)" % execute["map"][k] + last_name
-                        )
+                        new_name = (execute["new"][k] +
+                                    "(%s)" % execute["map"][k] + last_name)
                     else:
                         new_name = execute["new"][k] + last_name
                     name_list += [{"old": execute["old"][i], "new": new_name}]
@@ -267,9 +293,11 @@ def get_data():
 
 @app.route("/GetExecute", methods=["post"])
 def get_execute():
-    return jsonify(
-        {"map": execute["map"], "list": execute["list"], "new": execute["new"]}
-    )
+    return jsonify({
+        "map": execute["map"],
+        "list": execute["list"],
+        "new": execute["new"]
+    })
 
 
 @app.route("/GetVersion", methods=["post"])
